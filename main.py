f0d343152cb996fbbf900912eb1b9c2915c1afbf
simburto <tingseamus@gmail.com>
2024-09-21 04:30:05 -0400

    added code restart recovery, end function, permissions, logging, ux upgrades,

--- main.py
+++ main.py
@@ -1,86 +1,99 @@
+import json
 import os
+import time
+from multiprocessing import Process, Manager
+
+from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from multiprocessing import Process, Manager
-import time
-from dotenv import load_dotenv
-import logging
 
 load_dotenv()
 
-# Initialize Slack App
 app = App(token=os.getenv("SLACK_BOT_TOKEN"))
+POLL_FILE = "polls.json"
+POLL_PROCESSES_FILE = "poll_processes.json"
+poll_processes = {}
+POLL_PERMS = "perms.json"
+logfile = open("log.txt", "a")
 
-def create_poll(channel_id, question, options, emojis, duration, max_mentions, polls):
-    poll_id = len(polls)
-    poll_data = {'active': True, 'channel_id': channel_id}
+def is_valid_rq(say, polls, rq_channel_id, rq_user_id, rq_poll_id=None):
+    level = perms.get(rq_user_id, None)
 
-    poll_results = {}
+    if level is None:  # Not in whitelist
+        say(f":wompwomp2::wompwomp2: <@{rq_user_id}> you are not in the whitelist! :wompwomp2::wompwomp2:")
+        return False
 
-    # Create poll message
-    poll_message = f"*{question}*\n"
-    for option, emoji in zip(options, emojis):
-        poll_message += f":{emoji.strip()}:{option.strip()}\n"
+    # We're creating a poll, so check if we have a valid level to do this action and check if ID is valid
+    if rq_poll_id is None:
+        if level >= 0:
+            return True
 
-    result = app.client.chat_postMessage(
-        channel=channel_id,
-        text=poll_message
-    )
+    poll = polls.get(rq_poll_id, None)
 
-    poll_ts = result['ts']
-    poll_data['timestamp'] = poll_ts
+    if poll is None:
+        app.client.chat_postEphemeral(channel=rq_channel_id, user=rq_user_id, text=f":wompwomp2::wompwomp2: <@{rq_user_id}> Invalid Poll "
+                                                                                   f"ID! :wompwomp2::wompwomp2:")
+        return False
 
-    stripped_emojis = [emoji.strip().strip(':') for emoji in emojis]
-
-    # Add initial reactions
-    for emoji in stripped_emojis:
-        try:
-            app.client.reactions_add(
-                channel=channel_id,
-                name=emoji,
-                timestamp=poll_ts
-            )
-        except Exception as e:
-            logging.error(f"Failed to add reaction '{emoji}': {e}")
-
-    # Store poll metadata in shared dict
-    poll_data.update({
-        'options': options,
-        'emojis': stripped_emojis,
-        'results': poll_results,
-        'max_mentions': max_mentions,
-        'start_time': time.time(),
-        'duration': duration
-    })
-    polls[poll_id] = poll_data
-
-    # Start a loop to update the poll every minute
-    while poll_data['active']:
-        update_poll_results(channel_id, poll_id, polls)
-        time.sleep(60)  # Sleep for 60 seconds
-
-        # Check if poll duration has expired
-        elapsed_time = time.time() - poll_data['start_time']  # Corrected line
-        if elapsed_time >= duration * 3600:  # Convert hours to seconds
-            poll_data['active'] = False
-            break
+    if level == 1:
+        if poll['channel_id'] != rq_channel_id:
+            app.client.chat_postEphemeral(channel=rq_channel_id, user=rq_user_id, text=f":wompwomp2::wompwomp2: <@{rq_user_id}> You can't "
+                                                                                       f"end a poll from a different "
+                                                                                       f"channel! :wompwomp2::wompwomp2:")
+            return False
+        else:
+            return True
+    else:
+        say(f":wompwomp2::wompwomp2: <@{rq_user_id}> you are not authorized! :wompwomp2::wompwomp2:")
+        return False
 
 
-def update_poll_results(channel_id, poll_id, polls):
+def save_polls_to_file(polls):
+    with open(POLL_FILE, 'w') as file:
+        json_polls = {str(k): dict(v) for k, v in polls.items()}
+        json.dump(json_polls, file)
+
+
+def load_polls_from_file():
+    if os.path.exists(POLL_FILE):
+        with open(POLL_FILE, 'r') as file:
+            try:
+                json_polls = json.load(file)
+                return {int(k): v for k, v in json_polls.items()}
+            except json.JSONDecodeError:
+                return {}
+    return {}
+
+
+def save_poll_processes_to_file(poll_processes):
+    with open(POLL_PROCESSES_FILE, 'w') as file:
+        json.dump({str(k): v.pid for k, v in poll_processes.items()}, file)
+
+
+def load_poll_processes_from_file():
+    if os.path.exists(POLL_PROCESSES_FILE):
+        with open(POLL_PROCESSES_FILE, 'r') as file:
+            try:
+                return {int(k): v for k, v in json.load(file).items()}
+            except json.JSONDecodeError:
+                return {}
+    return {}
+
+
+def process_poll(polls, poll_id, channel_id):
     poll = polls[poll_id]
     stripped_emojis = poll['emojis']
     options = poll['options']
     max_mentions = poll['max_mentions']
     poll_results = poll['results']
 
-    # Fetch reactions for the poll message
     try:
         reaction = app.client.reactions_get(
             channel=channel_id,
             timestamp=poll['timestamp']
         )
     except Exception as e:
-        logging.error(f"Error fetching reactions: {e}")
+        logfile.write(f"Error fetching reactions: {e}")
         return
 
     for emoji in stripped_emojis:
@@ -90,7 +103,7 @@ def update_poll_results(channel_id, poll_id, polls):
             limit = max_mentions + 1 if max_mentions >= 0 else len(reaction_data['users'])
             for user in reaction_data['users'][:limit]:
                 user_info = app.client.users_info(user=user)
-                if user_info['user']['id'] != "U07ML8X2DE1":  # Ensure bot's own reactions are excluded
+                if user_info['user']['id'] != "U07ML8X2DE1":
                     user_mentions.append(f"<@{user_info['user']['id']}>")
 
             poll_results[emoji] = {
@@ -103,20 +116,104 @@ def update_poll_results(channel_id, poll_id, polls):
                 "users": "No votes"
             }
 
-    # Construct the result message
-    remaining_time = poll['duration'] * 3600 - (time.time() - poll['start_time'])
-    remaining_minutes = max(0, int(remaining_time // 60))
-    remaining_seconds = int(remaining_time % 60)
-    max_members_msg = "Max Members: " + (str(max_mentions) if max_mentions >= 0 else "No limit")
-    result_message = f"Poll Results (Time Remaining: {remaining_minutes}m {remaining_seconds}s, {max_members_msg}):\n"
-    if poll['duration'] <= 0:
-        result_message = f"Poll Results (Time Remaining: No time limit, {max_members_msg}):\n"
+    return poll, max_mentions, options, stripped_emojis, poll_results
+
+
+def update_poll_results(channel_id, poll_id, polls):
+    if polls.get(poll_id, None):
+        poll, max_mentions, options, stripped_emojis, poll_results = process_poll(polls, poll_id, channel_id)
+
+        remaining_time = poll['duration'] * 3600 - (time.time() - poll['start_time'])
+        remaining_minutes = max(0, int(remaining_time // 60))
+        remaining_seconds = int(remaining_time % 60)
+        max_members_msg = "Max Members: " + (str(max_mentions) if max_mentions >= 0 else "No limit")
+        result_message = f"Poll Results (Time Remaining: {remaining_minutes}m {remaining_seconds}s, {max_members_msg}):\n"
+        if poll['duration'] <= 0:
+            result_message = f"Poll Results (Time Remaining: No time limit, {max_members_msg}):\n"
+
+        for option, emoji in zip(options, stripped_emojis):
+            user_mentions = poll_results[emoji]['users']
+            result_message += f":{emoji}: {option.strip()}: {poll_results[emoji]['count']} votes ({user_mentions})\n"
+
+        try:
+            app.client.chat_update(
+                channel=channel_id,
+                ts=poll['timestamp'],
+                text=result_message
+            )
+        except Exception as e:
+            logfile.write(f"Failed to update message: {e}")
+
+
+def create_poll(channel_id, question, options, emojis, duration, max_mentions, polls, poll_id=None):
+    if poll_id is None:
+        poll_id = len(polls)
+
+    poll_data = polls.get(poll_id, {'active': True, 'channel_id': channel_id})
+    poll_results = poll_data.get('results', {})
+
+    if 'timestamp' not in poll_data:
+        poll_message = f"*{question}*\n"
+        for option, emoji in zip(options, emojis):
+            poll_message += f":{emoji.strip()}:{option.strip()}\n"
+
+        result = app.client.chat_postMessage(
+            channel=channel_id,
+            text=poll_message
+        )
+
+        poll_ts = result['ts']
+        poll_data['timestamp'] = poll_ts
+
+        stripped_emojis = [emoji.strip().strip(':') for emoji in emojis]
+
+        for emoji in stripped_emojis:
+            try:
+                app.client.reactions_add(
+                    channel=channel_id,
+                    name=emoji,
+                    timestamp=poll_ts
+                )
+            except Exception as e:
+                logfile.write(f"Failed to add reaction '{emoji}': {e}")
+
+        poll_data.update({
+            'options': options,
+            'emojis': stripped_emojis,
+            'results': poll_results,
+            'max_mentions': max_mentions,
+            'start_time': time.time(),
+            'duration': duration
+        })
+
+        polls[poll_id] = poll_data
+        save_polls_to_file(polls)
+        logfile.write(f"create poll {polls}")
+
+    while True:
+        if not polls.get(poll_id, None):
+            break
+
+        update_poll_results(channel_id, poll_id, polls)
+        time.sleep(10)
+
+        elapsed_time = time.time() - poll_data['start_time']
+        if elapsed_time >= duration * 3600 and duration > 0:
+            cleanup_poll(polls, poll_id, channel_id)
+            break
+
+    save_polls_to_file(polls)
+
+
+def cleanup_poll(polls, poll_id, channel_id):
+    poll, max_mentions, options, stripped_emojis, poll_results = process_poll(polls, poll_id, channel_id)
+
+    result_message = f"Final Poll Results:\n"
 
     for option, emoji in zip(options, stripped_emojis):
         user_mentions = poll_results[emoji]['users']
         result_message += f":{emoji}: {option.strip()}: {poll_results[emoji]['count']} votes ({user_mentions})\n"
 
-    # Update the poll message
     try:
         app.client.chat_update(
             channel=channel_id,
@@ -124,71 +221,92 @@ def update_poll_results(channel_id, poll_id, polls):
             text=result_message
         )
     except Exception as e:
-        logging.error(f"Failed to update message: {e}")
+        logfile.write(f"Failed to update message: {e}")
+
+    polls.pop(poll_id, None)
+    save_polls_to_file(polls)
 
 
 @app.command("/createpoll")
 def handle_createpoll(ack, body, say):
     ack()
     text = body['text']
-    try:
-        parts = [part.strip() for part in text.split('|')]
-        if len(parts) != 5:
-            say("Invalid format. Please use the format: question | option1,option2 | emoji1,emoji2 | duration (in hours) | max_mentions (number).")
-            return
-
-        question, options, emojis, duration, max_mentions = parts
-        options = options.split(',')
-        emojis = emojis.split(',')
-
+    channel_id = body['channel_id']
+    if is_valid_rq(say, polls, channel_id, body['user_id']):
         try:
-            duration = int(duration)
-        except ValueError:
-            say("Duration must be a valid number of hours.")
-            return
+            parts = [part.strip() for part in text.split('|')]
+            if len(parts) != 5:
+                app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text="Invalid format. Please use the format: "
+                                                                                             "question | option1,option2 | emoji1,"
+                                                                                             "emoji2 | duration (in hours) | max_mentions "
+                                                                                             "(number).")
+                return
 
-        try:
-            max_mentions = int(max_mentions)
-        except ValueError:
-            say("Max mentions must be a valid number.")
-            return
+            question, options, emojis, duration, max_mentions = parts
+            options = options.split(',')
+            emojis = emojis.split(',')
 
-        if len(options) != len(emojis):
-            say("The number of options must match the number of emojis.")
-            return
+            try:
+                duration = int(duration)
+            except ValueError:
+                app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text="Duration must be a valid number of hours.")
+                return
 
-        channel_id = body['channel_id']
-        p = Process(target=create_poll, args=(channel_id, question, options, emojis, duration, max_mentions, polls))
-        p.start()
+            try:
+                max_mentions = int(max_mentions)
+            except ValueError:
+                app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text="Max mentions must be a valid number.")
+                return
 
-        say(f"Poll created (ID: {len(polls)}): {question}")
+            if len(options) != len(emojis):
+                app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text="The number of options must match the number "
+                                                                                             "of emojis.")
+                return
 
-    except Exception as e:
-        say(f"Failed to create poll: {e}")
+            p = Process(target=create_poll, args=(channel_id, question, options, emojis, duration, max_mentions, polls))
+            p.start()
+
+            poll_id = len(polls)
+            poll_processes[poll_id] = p
+            save_poll_processes_to_file(poll_processes)
+            say(f"Poll created (ID: {poll_id}): {question}")
+
+        except Exception as e:
+            app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text=f":wompwomp2::wompwomp2: Failed to create poll: "
+                                                                                         f"{e} :wompwomp2::wompwomp2:")
 
 
-@app.command("/removepoll")
-def handle_removepoll(ack, body, say):
+@app.command("/endpoll")
+def handle_endpoll(ack, body, say):
     ack()
     poll_id_str = body['text'].strip()
-
+    channel_id = body['channel_id']
     if not poll_id_str.isdigit():
-        say("Please provide a valid poll ID.")
+        app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text="Please provide a valid poll ID.")
         return
-
     poll_id = int(poll_id_str)
+    if is_valid_rq(say, polls, channel_id, body['user_id'], poll_id):
+        if poll_id in polls:
+            cleanup_poll(polls, poll_id, channel_id)
 
-    # Check if the poll ID exists
-    if poll_id in polls:
-        polls[poll_id]['active'] = False  # Mark poll as inactive
-        say(f"Poll (ID: {poll_id}) has been removed successfully.")
-    else:
-        say(f"No active poll found with ID: {poll_id}.")
+            if poll_id in poll_processes:
+                process = poll_processes[poll_id]
+                process.terminate()
+                process.join()
+                del poll_processes[poll_id]
+                save_poll_processes_to_file(poll_processes)
+                app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text=f"Poll (ID: {poll_id}) has been ended "
+                                                                                             f"successfully.")
+            else:
+                app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text=f"Poll (ID: {poll_id}) is not running, but "
+                                                                                             f"has been deactivated.")
+        else:
+            app.client.chat_postEphemeral(channel=channel_id, user=body['user_id'], text=f"No active poll found with ID: {poll_id}.")
 
 
-# Reaction added event handler
 @app.event("reaction_added")
-def handle_reaction_added(body, say):
+def handle_reaction_added(ack, body):
+    ack()
     event = body['event']
     channel_id = event['item']['channel']
     poll_ts = event['item']['ts']
@@ -198,9 +316,10 @@ def handle_reaction_added(body, say):
             update_poll_results(channel_id, poll_id, polls)
             break
 
-# Reaction removed event handler
+
 @app.event("reaction_removed")
-def handle_reaction_removed(body, say):
+def handle_reaction_removed(ack, body):
+    ack()
     event = body['event']
     channel_id = event['item']['channel']
     poll_ts = event['item']['ts']
@@ -209,11 +328,38 @@ def handle_reaction_removed(body, say):
         if poll_data['timestamp'] == poll_ts:
             update_poll_results(channel_id, poll_id, polls)
             break
+
+
+def reload_active_polls():
+    for poll_id, poll_data in polls.items():
+        if poll_data['active']:
+            p = Process(target=create_poll, args=(
+                poll_data['channel_id'],
+                "",
+                poll_data['options'],
+                poll_data['emojis'],
+                poll_data['duration'],
+                poll_data['max_mentions'],
+                polls,
+                poll_id
+            ))
+            p.start()
+            poll_processes[poll_id] = p
+
+    save_poll_processes_to_file(poll_processes)
+
 
 if __name__ == "__main__":
-    # Make sure multiprocessing only runs in the main process
+    with open(POLL_PERMS, 'r') as file:
+        try:
+            perms = json.load(file)
+        except json.JSONDecodeError as e:
+            logfile.write(f"Failed to load permissions: {e}")
+
     manager = Manager()
-    polls = manager.dict()  # Global manager dict
+    polls = manager.dict(load_polls_from_file())
+    poll_processes = {}
+    reload_active_polls()
 
     handler = SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN"))
     handler.start()
